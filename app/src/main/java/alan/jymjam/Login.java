63947523424b0b7d09096a7fc4c5ffdbd1d557f5
Neeraj Prasad <nprasad@mit.edu>
2017-09-16 22:11:39 -0400

    Partial next views.

--- app/src/main/java/alan/jymjam/Login.java
+++ app/src/main/java/alan/jymjam/Login.java
@@ -1,7 +1,12 @@
 package alan.jymjam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class Login extends AppCompatActivity {
 
@@ -10,4 +15,13 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
