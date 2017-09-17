32c3d0e13fdf9b5a5253fd95e3e939fdc13ae0d0
Aaditya Singh <awesomeaaditya@gmail.com>
2017-09-16 22:19:55 -0400

    small edits.

--- app/src/main/java/alan/jymjam/Login.java
+++ app/src/main/java/alan/jymjam/Login.java
@@ -18,16 +18,6 @@ public class Login extends AppCompatActivity {
     private LoginButton loginButton;
     private CallbackManager callbackManager = CallbackManager.Factory.create();
 
-//    CallbackManager callbackManager;
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        callbackManager = CallbackManager.Factory.create();
-//        LoginButton loginButton = (LoginButton) view.findViewById(R.id.usersettings_fragment_login_button);
-//        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() { ... });
-//    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,39 +53,5 @@ public class Login extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-
-//    public static class LoginFragment extends Fragment {
-//        @Override
-//        public View onCreateView(
-//                LayoutInflater inflater,
-//                ViewGroup container,
-//                Bundle savedInstanceState) {
-//            View view = inflater.inflate(R.layout.splash, container, false);
-//
-//            loginButton = (LoginButton) view.findViewById(R.id.login_button);
-//            loginButton.setReadPermissions("email");
-//            // If using in a fragment
-//            loginButton.setFragment(this);
-//            // Other app specific specialization
-//
-//            // Callback registration
-//            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-//                @Override
-//                public void onSuccess(LoginResult loginResult) {
-//                    // App code
-//                }
-//
-//                @Override
-//                public void onCancel() {
-//                    // App code
-//                }
-//
-//                @Override
-//                public void onError(FacebookException exception) {
-//                    // App code
-//                }
-//            });
-//        }
-//    }
 
 }
